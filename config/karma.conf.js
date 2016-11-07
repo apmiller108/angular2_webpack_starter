6f498ec46ee91a2fe8b06976b1a66464abb01d63
Alex Miller <apmiller108@yahoo.com>
2016-11-06 21:47:21 -0500

    Configure karma to continuous testing and auto watch true

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -2,36 +2,29 @@ var webpackConfig = require('./webpack.test');
 
 module.exports = function (config) {
     var _config = {
+        autoWatch: true,
         basePath: '',
-
+        browsers: ['PhantomJS'],
+        colors: true,
         frameworks: ['jasmine'],
-
         files: [
             {pattern: './config/karma.entry.js', watched: false}
         ],
-
+        logLevel: config.LOG_INFO,
+        port: 9876,
         preprocessors: {
             './config/karma.entry.js': ['webpack', 'sourcemap']
         },
-
+        reporters: ['dots'],
+        singleRun: false,
         webpack: webpackConfig,
-
         webpackMiddleware: {
             stats: 'errors-only'
         },
-
         webpackServer: {
             noInfo: true
         },
 
-        reporters: ['progress'],
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: false,
-        browsers: ['PhantomJS'],
-        singleRun: true
     };
-
     config.set(_config);
 };
